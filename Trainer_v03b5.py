6d5ee1e7cfe82ca914203d24101ffa985e341edb
Milky <brickcao@outlook.com>
2023-07-18 17:51:50 +0900

    Update Trainer_v03b5.py

--- Trainer_v03b5.py
+++ Trainer_v03b5.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from Trainer_v03b3 import ImageEncoderM1, ImageDecoderM1
 from Trainer_v03b2 import *
 from TrainerTS import timer, MyDataset, split_loader, MyArgs, bn, Interpolate
@@ -27,6 +28,9 @@ class TrainerVTSM1(TrainerVTS):
             't_predict': {'Ground Truth': 'groundtruth',
                           'Estimated': 'predicts'
                           },
+            't_re_predict': {'Estimated': 'predicts',
+                             'Re-Estimated': 're_predicts'
+                             },
             't_test': {'Loss': 'loss',
                        'KL Loss': 'kl',
                        'Recon Loss': 'recon',
@@ -57,6 +61,7 @@ class TrainerVTSM1(TrainerVTS):
                        'kl': [],
                        'latent': [],
                        'predicts': [],
+                       're_predicts':[],
                        'groundtruth': []}
         return t_test_loss
 
@@ -169,3 +174,74 @@ class TrainerVTSM1(TrainerVTS):
 
             if idx % (len(loader)//5) == 0:
                 print("\rTeacher: {}/{} of test, loss={}".format(idx, len(loader), loss.item()), end='')
+
+    def plot_teacher_test(self, select_ind=None, select_num=8, autosave=False, notion=''):
+        self.__plot_settings__()
+        predict_items = self.plot_terms['t_predict']
+
+        # Depth Images
+        if select_ind:
+            inds = select_ind
+        else:
+            inds = np.random.choice(list(range(len(self.test_loss['t']['groundtruth']))), select_num, replace=False)
+        inds = np.sort(inds)
+
+        fig = plt.figure(constrained_layout=True)
+        fig.suptitle(f"Teacher Test Predicts @ep{self.train_loss['t']['epochs'][-1]}")
+        subfigs = fig.subfigures(nrows=2, ncols=1)
+
+        for i, item in enumerate(predict_items.keys()):
+            subfigs[i].suptitle(predict_items[item])
+            axes = subfigs[i].subplots(nrows=1, ncols=select_num)
+            for j in range(len(axes)):
+                img = axes[j].imshow(self.test_loss['t'][predict_items[item]][inds[j]], vmin=0, vmax=1)
+                axes[j].axis('off')
+                axes[j].set_title(f"#{inds[j]}")
+            subfigs[i].colorbar(img, ax=axes, shrink=0.8)
+
+        if autosave:
+            plt.savefig(f"{self.current_title()}_T_predict_{notion}.jpg")
+        plt.show()
+
+        predict_items = self.plot_terms['t_re_predict']
+        fig = plt.figure(constrained_layout=True)
+        fig.suptitle(f"Teacher Test Re-Predicts @ep{self.train_loss['t']['epochs'][-1]}")
+        subfigs = fig.subfigures(nrows=2, ncols=1)
+
+        for i, item in enumerate(predict_items.keys()):
+            subfigs[i].suptitle(predict_items[item])
+            axes = subfigs[i].subplots(nrows=1, ncols=select_num)
+            for j in range(len(axes)):
+                img = axes[j].imshow(self.test_loss['t'][predict_items[item]][inds[j]], vmin=0, vmax=1)
+                axes[j].axis('off')
+                axes[j].set_title(f"#{inds[j]}")
+            subfigs[i].colorbar(img, ax=axes, shrink=0.8)
+
+        if autosave:
+            plt.savefig(f"{self.current_title()}_T_re_predict_{notion}.jpg")
+        plt.show()
+
+        # Test Loss
+        loss_items = self.plot_terms['t_test']
+        fig = plt.figure(constrained_layout=True)
+        fig.suptitle(f"Teacher Test Loss @ep{self.train_loss['t']['epochs'][-1]}")
+        if len(loss_items.keys()) > 1:
+            axes = fig.subplots(1, len(loss_items.keys()))
+            axes = axes.flatten()
+        else:
+            axes = [plt.gca()]
+
+        for i, loss in enumerate(loss_items.keys()):
+            axes[i].scatter(list(range(len(self.test_loss['t']['groundtruth']))),
+                            self.test_loss['t'][loss_items[loss]], alpha=0.6)
+            axes[i].set_title(loss)
+            axes[i].set_xlabel('#Sample')
+            axes[i].set_ylabel('Loss')
+            axes[i].grid()
+            for j in inds:
+                axes[i].scatter(j, self.test_loss['t'][loss_items[loss]][j],
+                                c='magenta', marker=(5, 1), linewidths=4)
+
+        if autosave:
+            plt.savefig(f"{self.current_title()}_T_test_{notion}.jpg")
+        plt.show()
