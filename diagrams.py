87897fe3b453c33127a810e6eb1141db7cc60257
Milky <brickcao@outlook.com>
2023-09-01 11:31:18 +0900

    Update diagrams.py

--- diagrams.py
+++ diagrams.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-prop = np.load('../results/0829prop (1).npy')
+prop = np.load('../results/0829prop (2).npy')
 ae = np.load('../results/0829ae (1).npy')
 ts = np.load('../results/0829ts (1).npy')
 wi2vi = np.load('../results/0829wi2vi (1).npy')
@@ -14,17 +14,19 @@ yvals_ts = np.arange(len(sorted_ts))/float(len(sorted_ts))
 sorted_wi2vi = np.sort(wi2vi) / 4.6875
 yvals_wi2vi = np.arange(len(sorted_wi2vi))/float(len(sorted_wi2vi))
 
-#plt.plot(sorted_prop, yvals_prop, label='Proposed')
-#plt.plot(sorted_ae, yvals_ae, label='Encoder-Decoder')
-#plt.plot(sorted_ts, yvals_ts, label='Teacher-Student')
-#plt.plot(sorted_wi2vi, yvals_wi2vi, label='Wi2Vi')
+plt.plot(sorted_prop, yvals_prop, label='Proposed')
+plt.plot(sorted_ae, yvals_ae, label='Encoder-Decoder')
+plt.plot(sorted_ts, yvals_ts, label='Teacher-Student')
+plt.plot(sorted_wi2vi, yvals_wi2vi, label='Wi2Vi')
 
-#plt.xscale("log", base=2)
-#plt.legend()
-#plt.title('CDF of image loss (MSE)')
-#plt.xlabel('MSE per image')
-#plt.ylabel('Cumulative Freq')
+plt.xscale("log", base=2)
+plt.legend()
+plt.title('CDF of image loss (MSE)')
+plt.xlabel('MSE per image')
+plt.ylabel('Cumulative Freq')
+plt.show()
 
+plt.figure()
 plt.title('Boxplot of per-Image MSE Loss')
 plt.yscale('log', base=2)
 labels = ['Proposed', 'Encoder-Decoder', 'Teacher-Student', 'Wi2Vi']
