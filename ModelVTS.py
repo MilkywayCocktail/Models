9dc16373be2a33e1be6fd5dee647c2824a750c4f
Milky <brickcao@outlook.com>
2023-09-29 20:11:28 +0900

    update

--- ModelVTS.py
+++ ModelVTS.py
@@ -3,8 +3,36 @@ import torch.nn as nn
 from torchinfo import summary
 from TrainerTS import bn, Interpolate
 
+def reparameterize(mu, logvar):
+    eps = torch.randn_like(mu)
+    return mu + eps * torch.exp(logvar / 2)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect'),
+            # nn.BatchNorm2d(out_channels),
+            # nn.ReLU()
+            )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            # nn.BatchNorm2d(out_channels)
+            )
+
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out += residual
+        out = self.relu(out)
+        return out
+
 # ------------------------------------- #
-# Model v03b2
+# Model v03c1
 # VAE version; Adaptive to MNIST
 # Added interpolating decoder
 
@@ -13,56 +41,43 @@ from TrainerTS import bn, Interpolate
 # CSIEncoder: in = 2 * 90 * 100, out = 1 * 256 (Unused)
 
 
-def reparameterize(mu, logvar):
-    eps = torch.randn_like(mu)
-    return mu + eps * torch.exp(logvar / 2)
-
-
-class ImageEncoderV03b2(nn.Module):
+class ImageEncoderV03c1(nn.Module):
     def __init__(self, bottleneck='fc', batchnorm=False, latent_dim=8, active_func=nn.Tanh()):
-        super(ImageEncoderV03b2, self).__init__()
+        super(ImageEncoderV03c1, self).__init__()
 
         self.bottleneck = bottleneck
         self.latent_dim = latent_dim
         self.active_func = active_func
 
-        self.layer1 = nn.Sequential(
+        self.cnn = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
             bn(16, batchnorm),
             nn.LeakyReLU(inplace=True),
             # nn.MaxPool2d(2, stride=2)
             # In = 128 * 128 * 1
             # Out = 64 * 64 * 16
-        )
 
-        self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
             bn(32, batchnorm),
             nn.LeakyReLU(inplace=True),
             # nn.MaxPool2d(2, stride=2)
             # In = 64 * 64 * 16
             # Out = 32 * 32 * 32
-        )
 
-        self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
             bn(64, batchnorm),
             nn.LeakyReLU(inplace=True),
             # nn.MaxPool2d(2, stride=2)
             # In = 32 * 32 * 32
             # Out = 16 * 16 * 64
-        )
 
-        self.layer4 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # nn.MaxPool2d(2, stride=2)
             # In = 16 * 16 * 64
             # Out = 8 * 8 * 128
-        )
 
-        self.layer5 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
             bn(256, batchnorm),
             nn.LeakyReLU(inplace=True),
@@ -83,14 +98,10 @@ class ImageEncoderV03b2(nn.Module):
         )
 
     def __str__(self):
-        return 'ImgEnV03b2' + self.bottleneck.capitalize()
+        return 'ImgEnV03c1' + self.bottleneck.capitalize()
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
+        x = self.cnn(x)
 
         if self.bottleneck == 'fc':
             x = self.fclayers(x.view(-1, 4 * 4 * 256))
@@ -104,9 +115,9 @@ class ImageEncoderV03b2(nn.Module):
         return x, z
 
 
-class ImageDecoderV03b2(nn.Module):
+class ImageDecoderV03c1(nn.Module):
     def __init__(self, batchnorm=False, latent_dim=8, active_func=nn.Sigmoid()):
-        super(ImageDecoderV03b2, self).__init__()
+        super(ImageDecoderV03c1, self).__init__()
 
         self.latent_dim = latent_dim
         self.active_func = active_func
@@ -118,47 +129,37 @@ class ImageDecoderV03b2(nn.Module):
             nn.ReLU()
         )
 
-        self.layer1 = nn.Sequential(
+        self.cnn = nn.Sequential(
             nn.ConvTranspose2d(256, 128, kernel_size=4),
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 1 * 1 * 256
             # Out = 4 * 4 * 128
-        )
 
-        self.layer2 = nn.Sequential(
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             bn(64, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 4 * 4 * 128
             # Out = 8 * 8 * 64
-        )
 
-        self.layer3 = nn.Sequential(
             nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
             bn(32, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 8 * 8 * 64
             # Out = 16 * 16 * 32
-        )
 
-        self.layer4 = nn.Sequential(
             nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
             bn(16, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 16 * 16 * 32
             # Out = 32 * 32 * 16
-        )
 
-        self.layer5 = nn.Sequential(
             nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
             bn(8, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 32 * 32 * 16
             # Out = 64 * 64 * 8
-        )
 
-        self.layer6 = nn.Sequential(
             nn.ConvTranspose2d(8, 1, kernel_size=4, stride=2, padding=1),
             bn(1, batchnorm),
             self.active_func,
@@ -167,24 +168,18 @@ class ImageDecoderV03b2(nn.Module):
         )
 
     def __str__(self):
-        return 'ImgDeV03b2'
+        return 'ImgDeV03c1'
 
     def forward(self, z):
         z = self.fclayers(z)
-
-        z = self.layer1(z.view(-1, 256, 1, 1))
-        z = self.layer2(z)
-        z = self.layer3(z)
-        z = self.layer4(z)
-        z = self.layer5(z)
-        z = self.layer6(z)
+        z = self.cnn(z.view(-1, 256, 1, 1))
 
         return z.view(-1, 1, 128, 128)
 
 
-class ImageDecoderIntV03b2(ImageDecoderV03b2):
+class ImageDecoderIntV03c1(ImageDecoderV03c1):
     def __init__(self, batchnorm=False, latent_dim=8, active_func=nn.Sigmoid()):
-        super(ImageDecoderIntV03b2, self).__init__()
+        super(ImageDecoderIntV03c1, self).__init__()
 
         self.latent_dim = latent_dim
         self.active_func = active_func
@@ -232,7 +227,7 @@ class ImageDecoderIntV03b2(ImageDecoderV03b2):
         )
 
     def __str__(self):
-        return 'ImgDeV03b2Int'
+        return 'ImgDeV03c1Int'
 
     def forward(self, z):
         z = self.fclayers(z)
@@ -245,13 +240,14 @@ class ImageDecoderIntV03b2(ImageDecoderV03b2):
         return z.view(-1, 1, 128, 128)
 
 
-class CsiEncoderV03b2(nn.Module):
-    def __init__(self, bottleneck='last', batchnorm=False, latent_dim=8, active_func=nn.Sigmoid()):
-        super(CsiEncoderV03b2, self).__init__()
+class CsiEncoderV03c1(nn.Module):
+    def __init__(self, bottleneck='last', batchnorm=False, latent_dim=8, active_func=nn.Sigmoid(), feature_length=512):
+        super(CsiEncoderV03c1, self).__init__()
 
         self.bottleneck = bottleneck
         self.latent_dim = latent_dim
         self.active_func = active_func
+        self.feature_length = feature_length
 
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, stride=(3, 1), padding=0),
@@ -315,11 +311,11 @@ class CsiEncoderV03b2(nn.Module):
         )
 
         self.lstm = nn.Sequential(
-            nn.LSTM(512, 2 * self.latent_dim, 2, batch_first=True, dropout=0.1),
+            nn.LSTM(self.feature_length, 2 * self.latent_dim, 2, batch_first=True, dropout=0.1),
         )
 
     def __str__(self):
-        return 'CsiEnV03b2' + self.bottleneck.capitalize()
+        return 'CsiEnV03c1' + self.bottleneck.capitalize()
 
     def forward(self, x):
         x = torch.chunk(x.view(-1, 2, 90, 100), 2, dim=1)
@@ -336,7 +332,8 @@ class CsiEncoderV03b2(nn.Module):
         x2 = self.layer5(x2)
 
         out = torch.cat([x1, x2], dim=1)
-        out, (final_hidden_state, final_cell_state) = self.lstm.forward(out.view(-1, 512, 8 * 42).transpose(1, 2))
+        out, (final_hidden_state, final_cell_state) = self.lstm.forward(
+            out.view(-1, self.feature_length, 8 * 42).transpose(1, 2))
 
         if self.bottleneck == 'last':
             out = out[:, -1, :]
@@ -346,8 +343,8 @@ class CsiEncoderV03b2(nn.Module):
 
         return out, z
 
-# Model v03b3
-# Minor modifications to Model v03b2
+# Model v03c2
+# Minor modifications to Model v03c1
 # In number of channels
 
 # ImageEncoder: in = 128 * 128, out = 1 * latent_dim
@@ -355,9 +352,9 @@ class CsiEncoderV03b2(nn.Module):
 # CSIEncoder: in = 2 * 90 * 100, out = 1 * 256 (Unused)
 
 
-class ImageEncoderV03b3(ImageEncoderV03b2):
+class ImageEncoderV03c2(ImageEncoderV03c1):
     def __init__(self, bottleneck='fc', batchnorm=False, latent_dim=8, active_func=nn.Tanh()):
-        super(ImageEncoderV03b3, self).__init__(bottleneck, batchnorm, latent_dim, active_func)
+        super(ImageEncoderV03c2, self).__init__(bottleneck, batchnorm, latent_dim, active_func)
 
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 128, kernel_size=3, stride=2, padding=1),
@@ -416,12 +413,12 @@ class ImageEncoderV03b3(ImageEncoderV03b2):
         )
 
     def __str__(self):
-        return 'ImgEnV03b3' + self.bottleneck.capitalize()
+        return 'ImgEnV03c2' + self.bottleneck.capitalize()
 
 
-class ImageDecoderV03b3(ImageDecoderV03b2):
+class ImageDecoderV03c2(ImageDecoderV03c1):
     def __init__(self, batchnorm=False, latent_dim=8, active_func=nn.Sigmoid()):
-        super(ImageDecoderV03b3, self).__init__(batchnorm, latent_dim, active_func)
+        super(ImageDecoderV03c2, self).__init__(batchnorm, latent_dim, active_func)
 
         self.fclayers = nn.Sequential(
             nn.Linear(self.latent_dim, 4096),
@@ -436,33 +433,25 @@ class ImageDecoderV03b3(ImageDecoderV03b2):
             nn.LeakyReLU(inplace=True),
             # In = 4 * 4 * 128
             # Out = 8 * 8 * 128
-        )
 
-        self.layer2 = nn.Sequential(
             nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 8 * 8 * 128
             # Out = 16 * 16 * 128
-        )
 
-        self.layer3 = nn.Sequential(
             nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 16 * 16 * 128
             # Out = 32 * 32 * 128
-        )
 
-        self.layer4 = nn.Sequential(
             nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # In = 32 * 32 * 128
             # Out = 64 * 64 * 128
-        )
 
-        self.layer5 = nn.Sequential(
             nn.ConvTranspose2d(128, 1, kernel_size=4, stride=2, padding=1),
             bn(1, batchnorm),
             self.active_func,
@@ -471,18 +460,208 @@ class ImageDecoderV03b3(ImageDecoderV03b2):
         )
 
     def __str__(self):
-        return 'ImgDeV03b3'
+        return 'ImgDeV03c2'
 
-    def forward(self, z):
-        z = self.fclayers(z)
 
-        z = self.layer1(z.view(-1, 128, 4, 4))
-        z = self.layer2(z)
-        z = self.layer3(z)
-        z = self.layer4(z)
-        z = self.layer5(z)
+class ImageEncoderV03c3(ImageEncoderV03c2):
+    def __init__(self, bottleneck='fc', batchnorm=False, latent_dim=8, active_func=nn.Tanh()):
+        super(ImageEncoderV03c3, self).__init__(bottleneck, batchnorm, latent_dim, active_func)
 
-        return z.view(-1, 1, 128, 128)
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1),
+            bn(32, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # nn.MaxPool2d(2, stride=2)
+            # In = 128 * 128 * 1
+            # Out = 64 * 64 * 32
+
+            ResidualBlock(32, 32),
+            ResidualBlock(32, 32),
+            ResidualBlock(32, 32),
+
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            bn(64, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # nn.MaxPool2d(2, stride=2)
+            # In = 64 * 64 * 32
+            # Out = 32 * 32 * 64
+
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            bn(128, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # nn.MaxPool2d(2, stride=2)
+            # In = 32 * 32 * 64
+            # Out = 16 * 16 * 128
+
+            ResidualBlock(128, 128),
+            ResidualBlock(128, 128),
+            ResidualBlock(128, 128),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            bn(256, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # nn.MaxPool2d(2, stride=2)
+            # In = 16 * 16 * 128
+            # Out = 8 * 8 * 256
+
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+            ResidualBlock(256, 256),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+            bn(512, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # nn.MaxPool2d(2, stride=2)
+            # In = 8 * 8 * 256
+            # Out = 4 * 4 * 512
+
+            ResidualBlock(512, 512),
+            ResidualBlock(512, 512)
+        )
+
+        self.fclayers = nn.Sequential(
+            nn.Linear(4 * 4 * 512, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 2 * self.latent_dim),
+            self.active_func
+        )
+
+    def __str__(self):
+        return 'ImgEnV03c3' + self.bottleneck.capitalize()
+
+    def forward(self, x):
+        x = self.cnn(x)
+
+        if self.bottleneck == 'fc':
+            x = self.fclayers(x.view(-1, 4 * 4 * 512))
+        elif self.bottleneck == 'gap':
+            x = self.gap(x)
+            x = nn.Sigmoid(x)
+
+        mu, logvar = x.view(-1, 2 * self.latent_dim).chunk(2, dim=-1)
+        z = reparameterize(mu, logvar)
+
+        return x, z
+
+
+class ImageDecoderV03c3(ImageDecoderV03c2):
+    def __init__(self, batchnorm=False, latent_dim=8, active_func=nn.Sigmoid()):
+        super(ImageDecoderV03c2, self).__init__(batchnorm, latent_dim, active_func)
+
+        self.cnn = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4),
+            bn(128, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # In = 1 * 1 * 256
+            # Out = 4 * 4 * 128
+
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            bn(64, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # In = 4 * 4 * 128
+            # Out = 8 * 8 * 64
+
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            bn(32, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # In = 8 * 8 * 64
+            # Out = 16 * 16 * 32
+
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
+            bn(16, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # In = 16 * 16 * 32
+            # Out = 32 * 32 * 16
+
+            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
+            bn(8, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # In = 32 * 32 * 16
+            # Out = 64 * 64 * 8
+
+            nn.ConvTranspose2d(8, 1, kernel_size=4, stride=2, padding=1),
+            bn(1, batchnorm),
+            self.active_func,
+            # In = 64 * 64 * 8
+            # Out = 128 * 128 * 1
+        )
+
+
+class CsiEncoderV03c3(CsiEncoderV03c1):
+    def __init__(self, bottleneck='last', batchnorm=False, latent_dim=8, active_func=nn.Sigmoid(), feature_length=1024):
+        super(CsiEncoderV03c3, self).__init__(bottleneck, batchnorm, latent_dim, active_func, feature_length)
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=(3, 1), padding=0),
+            bn(16, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # No Padding
+            # No Pooling
+            # In = 90 * 100 * 1
+            # Out = 30 * 98 * 16
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 64, kernel_size=3, stride=(2, 2), padding=0),
+            bn(64, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # No Padding
+            # No Pooling
+            # In = 30 * 98 * 16
+            # Out = 14 * 48 * 64
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=(1, 1), padding=0),
+            bn(128, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # No Padding
+            # No Pooling
+            # In = 14 * 48 * 64
+            # Out = 12 * 46 * 128
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=(1, 1), padding=0),
+            bn(256, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # No Padding
+            # No Pooling
+            # In = 12 * 46 * 128
+            # Out = 10 * 44 * 256
+        )
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=(1, 1), padding=0),
+            bn(512, batchnorm),
+            nn.LeakyReLU(inplace=True),
+            # No Padding
+            # No Pooling
+            # In = 10 * 44 * 256
+            # Out = 8 * 42 * 512
+        )
+
+        self.gap = nn.Sequential(
+            nn.AvgPool1d(kernel_size=8 * 42, stride=1, padding=0)
+        )
+
+        self.fclayers = nn.Sequential(
+            nn.Linear(512 * 8 * 42, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 256),
+            nn.ReLU()
+        )
+
+        self.lstm = nn.Sequential(
+            nn.LSTM(self.feature_length, 2 * self.latent_dim, 2, batch_first=True, dropout=0.1),
+        )
+
+    def __str__(self):
+        return 'CsiEnV03c3' + self.bottleneck.capitalize()
 
 
 if __name__ == "__main__":
@@ -492,5 +671,5 @@ if __name__ == "__main__":
     # summary(m2, input_size=(1, 16))
     # m3 = CsiEncoder(batchnorm=False)
     # summary(m3, input_size=(2, 90, 100))
-    m4 = ImageDecoderIntV03b2(latent_dim=16)
-    summary(m4, input_size=(1, 16))
+    m4 = ImageEncoderV03c3(latent_dim=16)
+    summary(m4, input_size=(1, 128, 128))
