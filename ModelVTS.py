705a38473db6db97fb305ec9a30bd4d02f7cfc82
Milky <brickcao@outlook.com>
2023-12-15 13:59:33 +0900

    Update ModelVTS.py

--- ModelVTS.py
+++ ModelVTS.py
@@ -996,7 +996,7 @@ class ImageDecoderV04c2(ImageDecoderV03c2):
             bn(128, batchnorm),
             nn.LeakyReLU(inplace=True),
             # 128 * 128 * 128
-            nn.Conv2d(128, out_channels=1, kernel_size=3, stride=11, padding=1),
+            nn.Conv2d(128, out_channels=1, kernel_size=3, stride=1, padding=1),
             self.active_func,
             # 1 * 128 * 128
         )
