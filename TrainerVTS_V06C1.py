56a68d81f46ed1b2196fe072b006fc4dd608d277
Milky <brickcao@outlook.com>
2024-02-14 13:09:57 +0900

    Update TrainerVTS_V06C1.py

--- TrainerVTS_V06C1.py
+++ TrainerVTS_V06C1.py
@@ -193,11 +193,10 @@ class TeacherTrainer(BasicTrainer):
         if select_ind:
             inds = select_ind
         else:
-            if self.inds is not None:
-                if select_num >= len(self.inds):
-                    inds = self.inds
-                else:
-                    inds = self.generate_indices(self.loss.loss['pred']['IND'], select_num)
+            if not any(self.inds):
+                inds = self.generate_indices(self.loss.loss['pred']['IND'], select_num)
+            else:
+                inds = self.inds
 
         fig1 = self.loss.plot_predict(title['PRED'], inds, ('GT', 'PRED'))
         fig2 = self.loss.plot_latent(title['LAT'], inds)
@@ -269,10 +268,10 @@ class StudentTrainer(BasicTrainer):
         if select_ind:
             inds = select_ind
         else:
-            if self.inds is not None and select_num == len(self.inds):
-                inds = self.inds
+            if not any(self.inds):
+                inds = self.generate_indices(self.loss.loss['pred']['IND'], select_num)
             else:
-                inds = self.generate_indices(self.loss['s'].loss['pred']['IND'], select_num)
+                inds = self.inds
 
         fig1 = self.loss.plot_predict(title['PRED'], inds, ('GT', 'T_PRED', 'S_PRED'))
         fig3 = self.loss.plot_latent(title['LATENT'], inds, ('T_LATENT', 'S_LATENT'))
@@ -322,10 +321,10 @@ class StudentTrainerBBX(StudentTrainer):
         if select_ind:
             inds = select_ind
         else:
-            if self.inds is not None and select_num == len(self.inds):
-                inds = self.inds
+            if not any(self.inds):
+                inds = self.generate_indices(self.loss.loss['pred']['IND'], select_num)
             else:
-                inds = self.generate_indices(self.loss['s'].loss['pred']['IND'], select_num)
+                inds = self.inds
 
         fig2 = self.loss['s'].plot_bbx(title['BBX'], inds)
 
