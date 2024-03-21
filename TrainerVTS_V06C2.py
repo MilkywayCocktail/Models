35a7b4b8053cb11cf3f23c49ad8c80415a0235d2
Milky <brickcao@outlook.com>
2024-03-21 15:59:05 +0900

    Update TrainerVTS_V06C2.py

--- TrainerVTS_V06C2.py
+++ TrainerVTS_V06C2.py
@@ -121,7 +121,8 @@ class BBXDecoder(nn.Module):
             nn.Linear(feature_length, 1024),
             # nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Linear(1024, 5)
+            nn.Linear(1024, 5),
+            nn.ReLU()
         )
 
     def __str__(self):
