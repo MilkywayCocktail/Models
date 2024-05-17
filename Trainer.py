143414ec7e315c9e874fa621747187d909003443
Milky <brickcao@outlook.com>
2024-05-17 14:29:41 +0900

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -203,9 +203,9 @@ class BasicTrainer:
                 if not os.path.exists(self.save_path):
                     os.makedirs(self.save_path)
 
-                with open(f"{self.save_path}{self.name}.txt", 'w') as logfile:
+                with open(f"{self.save_path}{self.name}_trained.txt", 'w') as logfile:
                     logfile.write(f"{notion}_{self.name}\n"
-                                  f"Total epochs = {self.current_ep()}"
+                                  f"Total epochs = {self.current_ep()}\n"
                                   f"Best : val_loss={self.best_val_loss} @ epoch {self.best_vloss_ep}\n"
                                   f"Modules:\n{list(self.models.values())}\n"
                                   )
